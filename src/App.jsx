f4711e994ae0844e3c8cbec9476554bb2b4804cf
geekyhimanshu2309 <himanshu112760@gmail.com>
2024-01-27 00:40:45 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+function App() {
+
+  return (
+    <div>
+      <h1 className="text-3xl flex justify-center w-full text-white underline bg-orange-500 z-10">Currency App</h1>
+    </div>
+  );
+}
+
+export default App
